b006fd8fa32b8c24c5693fe39165685fb31c8a4d
bibidibu <davaabatbilegt@gmail.com>
2022-04-22 14:55:22 +0800

    Lab4

---
+++ src/lab4/GradeManagerTest.java
@@ -0,0 +1,27 @@
+package lab4;
+
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class GradeManagerTest {
+
+	@Test public void makeHashMap() throws Exception {
+		GradeManager g = new GradeManager();
+		
+		Assert.assertNotNull(g);
+	}
+	@Test public void addValue() throws Exception {
+		GradeManager g = new GradeManager();
+		g.addGrade("A");
+		
+		Assert.assertNotNull(g);
+	}
+	@Test public void throwException() throws Exception {
+		GradeManager g = new GradeManager();
+		
+		Assert.assertThrows(null, null);
+		Assert.assertNotNull(g);
+	}
+}
---
+++ src/lab4/InvalidGradeException.java
@@ -0,0 +1,11 @@
+package lab4;
+
+/**
+ * Exception that should be thrown if an invalid grade is passed in.
+ *
+ */
+public class InvalidGradeException extends Exception {
+	public InvalidGradeException(String errorMessage) {
+		super(errorMessage);
+	}
+}
